7b32ac88ce62e3e928eca0336cbccab9ef0ad3dd
Jingwei Zhang <jingwei.zhang96@outlook.com>
2022-07-25 05:19:30 +0000

    fix build error

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,7 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/hehua-bamboo.png"
     }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
